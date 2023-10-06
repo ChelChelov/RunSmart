b747697390ce2a25952c7154d127c5349200d343
Oleg PC <sazonov1994@gmail.com>
2023-10-06 22:33:55 +0300

    update carousel(add buttons: prev, next), script.js(use tiny-slider); add styles into tns-item

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,36 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 500,
-        // adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left_arrow.svg"></img></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right_arrow.svg"></img></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    dots: true,
-                    arrows: false
-                }
-            }
-        ]
-      });
+// $(document).ready(function(){
+//     $('.carousel__inner').slick({
+//         speed: 500,
+//         // adaptiveHeight: true,
+//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left_arrow.svg"></img></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="icons/right_arrow.svg"></img></button>',
+//         responsive: [
+//             {
+//                 breakpoint: 992,
+//                 settings: {
+//                     dots: true,
+//                     arrows: false
+//                 }
+//             }
+//         ]
+//       });
+//   });
+
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false
   });
+
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+}); 
+
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+}); 
+    
+  
