acb4a59046460eb64ac35a7133e0246baf6731d1
Oleg PC <sazonov1994@gmail.com>
2023-10-24 19:09:18 +0300

    add phone-mask(validation)

--- src/js/script.js
+++ src/js/script.js
@@ -119,4 +119,6 @@ $(document).ready(function() {
     validateForms('#consultation-form');
     validateForms('#consultation form');
     validateForms('#order form');
+
+    $("input[name=phone]").mask("+38 (999) 999-99-99");
 });
