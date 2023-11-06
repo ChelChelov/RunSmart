79caf0ed78d250ea7110a14b5c881f6978aace25
Oleg PC <sazonov1994@gmail.com>
2023-11-06 04:24:05 +0200

    update document(website validation)

--- src/js/script.js
+++ src/js/script.js
@@ -160,5 +160,14 @@ $(document).ready(function() {
 
     //WOW
 
-    new WOW().init();
+    wow = new WOW(
+        {
+        boxClass:     'wow',      // default
+        animateClass: 'animated', // default
+        offset:       0,          // default
+        mobile:       false,       // default
+        live:         true        // default
+      }
+      )
+      wow.init();
 });
